2fea2dc201df51540d22812d7f8545ffee056001
Max-Lev <maxlevtov@gmail.com>
2023-06-22 16:22:44 +0300

    /api/v1

---
+++ functions/src/create.ts
@@ -0,0 +1,29 @@
+// interface User {
+//     firstName: String,
+//     lastName: String,
+//     email: String,
+//     areaNumber: String,
+//     department: String,
+//     id: String,
+//     contactNumber: String
+// }
+
+// // Create new user
+// app.post('/users', async (req, res) => {
+//     try {
+//         const user: User = {
+//             firstName: req.body['firstName'],
+//             lastName: req.body['lastName'],
+//             email: req.body['email'],
+//             areaNumber: req.body['areaNumber'],
+//             department: req.body['department'],
+//             id: req.body['id'],
+//             contactNumber: req.body['contactNumber']
+//         }
+
+//         const newDoc = await db.collection(userCollection).add(user);
+//         res.status(201).send(`Created a new user: ${newDoc.id}`);
+//     } catch (error) {
+//         res.status(400).send(`User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`)
+//     }
+// });
